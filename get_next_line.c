dce0f2288a9ac6f2f41b971adb0fa75d09330f89
atamas <atamas@student.42wolfsburg.de>
2024-01-03 14:45:13 +0100

    solve return NULL instead of empty str

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 16:59:22 by atamas            #+#    #+#             */
-/*   Updated: 2024/01/03 14:09:32 by atamas           ###   ########.fr       */
+/*   Updated: 2024/01/03 14:44:01 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,26 @@
 
 int	ft_strlen(const char *string)
 {
-    int	len;
+	int	len;
 
-    len = 0;
-    if (!string)
-        return (0);
-    while (string[len] != '\0')
-        len++;
-    return (len);
+	len = 0;
+	if (!string)
+		return (0);
+	while (string[len] != '\0')
+		len++;
+	return (len);
 }
 
 char	*create_copy_return(char *string, char *temp)
 {
 	char	*joined;
-	int		len_string = ft_strlen(string);
-	int		len_temp = ft_strlen(temp);
+	int		len_string;
+	int		len_temp;
 	int		i;
 	int		j;
 
+	len_string = ft_strlen(string);
+	len_temp = ft_strlen(temp);
 	i = -1;
 	j = 0;
 	joined = malloc(sizeof(char) * (len_string + len_temp + 1));
@@ -51,31 +53,27 @@ char	*create_copy_return(char *string, char *temp)
 		joined[i] = string[i];
 	while (j < len_temp)
 		joined[i++] = temp[j++];
-	free(string);
-	string = NULL;
-	free(temp);
-	temp = NULL;
-	return (joined);
+	return (free(string), free(temp), string = NULL, temp = NULL, joined);
 }
 
 char	*ft_strchr(const char *string, int c)
 {
-    char	ch;
+	char	ch;
 
-    ch = (char)c;
-    if (!string)
-        return (NULL);
-    while (*string != '\0')
-    {
-        if (*string == ch)
-        {
-            return ((char *)string);
-        }
-        string++;
-    }
-    if (ch == '\0')
-        return ((char *)string);
-    return (NULL);
+	ch = (char)c;
+	if (!string)
+		return (NULL);
+	while (*string != '\0')
+	{
+		if (*string == ch)
+		{
+			return ((char *)string);
+		}
+		string++;
+	}
+	if (ch == '\0')
+		return ((char *)string);
+	return (NULL);
 }
 
 char	*ft_static_handler(char	*static_str, char *string)
@@ -87,15 +85,10 @@ char	*ft_static_handler(char	*static_str, char *string)
 	i = 0;
 	j = -1;
 	if (!string)
-	{
-		free(static_str);
-		static_str = NULL;
-		return (NULL);
-	}
+		return (free(static_str), static_str = NULL, NULL);
 	i = ft_strlen(string);
 	if (i > 0 && string[i - 1] == '\n')
 	{
-
 		memory = malloc(sizeof(char) * (ft_strlen(static_str + i) + 1));
 		while (static_str[++j + i] != '\0')
 			memory[j] = static_str[j + i];
@@ -105,10 +98,7 @@ char	*ft_static_handler(char	*static_str, char *string)
 		return (memory);
 	}
 	else
-	{
-		free(static_str);
-		return (NULL);
-	}
+		return (free(static_str), NULL);
 }
 
 char	*ft_copy_until_n(char *string)
@@ -135,25 +125,22 @@ char	*ft_copy_until_n(char *string)
 	return (copied);
 }
 
-char	*ft_read(int fd, char	*string)
+char	*ft_read(int fd, char	*my_static)
 {
 	char	*temp;
 	int		read_val;
 
 	read_val = 1;
-	while (ft_strchr(string, '\n') == NULL && read_val > 0)
+	while (ft_strchr(my_static, '\n') == NULL && read_val > 0)
 	{
 		temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 		read_val = read(fd, temp, BUFFER_SIZE);
 		temp[read_val] = '\0';
 		if (read_val == -1)
-		{
-			free(temp);
-			return (NULL);
-		}
-		string = create_copy_return(string, temp);
+			return (free(temp), free(my_static), NULL);
+		my_static = create_copy_return(my_static, temp);
 	}
-	return (string);
+	return (my_static);
 }
 
 char	*get_next_line(int fd)
@@ -162,12 +149,14 @@ char	*get_next_line(int fd)
 	static char	*my_static;
 
 	if (read(fd, 0, 0) < 0 || fd < 0 || BUFFER_SIZE <= 0)
-	{
 		return (NULL, my_static = NULL);
-	}
 	my_static = ft_read(fd, my_static);
+	if (my_static == NULL)
+		return (NULL);
 	line_read = ft_copy_until_n(my_static);
 	my_static = ft_static_handler(my_static, line_read);
+	if (line_read[0] == '\0')
+		return (free(line_read), NULL);
 	return (line_read);
 }
 
